4aa1e5a0db39d7d0b399f24cbbddaf4777e2e7ef
Gabriel <dev123gabriel@gmail.com>
2024-01-24 17:08:00 -0300

    refactor: validação de data inicial e final

--- src/validation/eventSchema.ts
+++ src/validation/eventSchema.ts
@@ -1,18 +1,32 @@
 import { dateIsValid } from "@/utils";
 import { z } from "zod";
 
-export const validationSchema = z.object({
-  title: z.string().min(1, "Campo obrigatório"),
-  description: z.string().min(1, "Campo obrigatório"),
-  start: z.string().min(1, "Campo obrigatório").refine(dateIsValid, {
-    message: "Data inválida",
-  }),
-  end: z
-    .string()
-    .refine((value) => (value ? dateIsValid(value) : true), {
+export const validationSchema = z
+  .object({
+    title: z.string().min(1, "Campo obrigatório"),
+    description: z.string().min(1, "Campo obrigatório"),
+    start: z.string().min(1, "Campo obrigatório").refine(dateIsValid, {
       message: "Data inválida",
-    })
-    .optional(),
-  url: z.string().url("Informe uma url válida").optional(),
-  backgroundColor: z.string().optional(),
-});
+    }),
+    end: z
+      .string()
+      .optional()
+      .refine((value) => (value ? dateIsValid(value) : true), {
+        message: "Data inválida",
+      }),
+    url: z.string().url("Informe uma url válida").optional(),
+    backgroundColor: z.string().optional(),
+  })
+  .refine(
+    ({ start, end }) => {
+      if (end && new Date(start).getTime() > new Date(end).getTime()) {
+        return false;
+      }
+
+      return true;
+    },
+    {
+      message: "A data de início deve ser maior que a de início",
+      path: ["start"],
+    }
+  );
