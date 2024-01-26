16c174fecd44e0704a708d611e2426e56607b504
Gabriel <dev123gabriel@gmail.com>
2024-01-26 10:04:43 -0300

    :recycle: refactor: title e description validados corretamente

--- src/validation/taskSchema.ts
+++ src/validation/taskSchema.ts
@@ -2,6 +2,12 @@ import { z } from "zod";
 
 export const validationSchema = z.object({
   tag: z.enum(["studie", "work", "exercise", "other"]),
-  title: z.string().min(1, "Campo obrigatório"),
-  description: z.string().min(1, "Campo obrigatório"),
+  title: z
+    .string()
+    .min(1, "Campo obrigatório")
+    .max(30, "O título deve ter no máximo 30 caracteres"),
+  description: z
+    .string()
+    .min(1, "Campo obrigatório")
+    .max(165, "A descricão deve ter no máximo 165 caracteres"),
 });
